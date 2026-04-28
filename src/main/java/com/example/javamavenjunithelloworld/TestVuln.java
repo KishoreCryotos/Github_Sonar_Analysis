c37a6e17458d46eb197892645221002630e6e8d6
KishoreCryotos <kishore.k@cryotos.com>
2026-04-28 17:00:39 +0530

    Update TestVuln.java

--- src/main/java/com/example/javamavenjunithelloworld/TestVuln.java
+++ src/main/java/com/example/javamavenjunithelloworld/TestVuln.java
@@ -8,11 +8,22 @@ public class TestVuln {
     String awsKey = "AKIA123456789TESTKEY";
     String password = "admin123";
 
+    String githubToken = "ghp_1234567890abcdefghijklmnop";
+    String apiKey = "sk_test_ABC123SECRETXYZ";
+    String dbPassword = "root@123";
+
     public void run(String input, Connection conn) throws Exception {
 
         Runtime.getRuntime().exec(input);
 
+        Runtime.getRuntime().exec("cmd /c " + input);
+
         Statement stmt = conn.createStatement();
         stmt.executeQuery("SELECT * FROM users WHERE id=" + input);
+
+        stmt.executeQuery("DELETE FROM users WHERE name='" + input + "'");
+
+        String query = "UPDATE users SET role='admin' WHERE id=" + input;
+        stmt.executeQuery(query);
     }
 }
