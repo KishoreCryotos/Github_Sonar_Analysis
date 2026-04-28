ec706631f23806bae2ba659eaabdcb88306323af
KishoreCryotos <kishore.k@cryotos.com>
2026-04-28 17:14:18 +0530

    Update TestVuln.java

--- src/main/java/com/example/javamavenjunithelloworld/TestVuln.java
+++ src/main/java/com/example/javamavenjunithelloworld/TestVuln.java
@@ -12,13 +12,20 @@ public class TestVuln {
     String apiKey = "sk_test_ABC123SECRETXYZ";
     String dbPassword = "root@123";
 
+    // New secret for extra Gitleaks finding
+    String awsKey2 = "AKIA999999999NEWTEST";
+
     public void run(String input, Connection conn) throws Exception {
 
         Runtime.getRuntime().exec(input);
 
         Runtime.getRuntime().exec("cmd /c " + input);
 
+        // New Semgrep finding
+        Runtime.getRuntime().exec("powershell " + input);
+
         Statement stmt = conn.createStatement();
+
         stmt.executeQuery("SELECT * FROM users WHERE id=" + input);
 
         stmt.executeQuery("DELETE FROM users WHERE name='" + input + "'");
